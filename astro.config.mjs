9da19af51923d9d51395cc6d0a9301316f04e315
Vladimir Figueroa <vladimir.antonio.f@gmail.com>
2024-10-11 15:39:16 -0300

    fix: Vercel Web Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact({ compat: true }), tailwind()],
   server: { port: 4680 },
-  output: 'server',
+  output: 'static',
   adapter: vercel({
     webAnalytics: { enabled: true }
   }),
