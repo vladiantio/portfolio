9db43ac0bc3c6fc0ed50348e766ecc44d1e80a80
Vladimir Antonio F. C. <50648871+vladiantio@users.noreply.github.com>
2025-01-11 17:30:09 -0300

    ⚙️ Add custom Tailwind CSS shadow utilities

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -22,6 +22,20 @@ export default {
 			fontFamily: {
 				sans: ["var(--font-sans)", ...defaultTheme.fontFamily.sans],
 			},
+			boxShadow: {
+				'center-sm':	'0 0 2px 0 rgb(0 0 0 / 0.05)',
+				'center':	'0 0 3px 0 rgb(0 0 0 / 0.1), 0 0 2px -1px rgb(0 0 0 / 0.1)',
+				'center-md':	'0 0 6px -1px rgb(0 0 0 / 0.1), 0 0 4px -2px rgb(0 0 0 / 0.1)',
+				'center-lg':	'0 0 15px -3px rgb(0 0 0 / 0.1), 0 0 6px -4px rgb(0 0 0 / 0.1)',
+				'center-xl':	'0 0 25px -5px rgb(0 0 0 / 0.1), 0 0 10px -6px rgb(0 0 0 / 0.1)',
+				'center-2xl':	'0 0 50px -12px rgb(0 0 0 / 0.25)',
+				'neon-sm':	'0 0 2px 0 rgb(0 0 0 / 0.05), inset 0 0 1px 0 rgb(0 0 0 / 0.05)',
+				'neon':	'0 0 3px 0 rgb(0 0 0 / 0.1), 0 0 2px -1px rgb(0 0 0 / 0.1), inset 0 0 2px 0 rgb(0 0 0 / 0.1)',
+				'neon-md':	'0 0 6px -1px rgb(0 0 0 / 0.1), 0 0 4px -2px rgb(0 0 0 / 0.1), inset 0 0 3px 0 rgb(0 0 0 / 0.1)',
+				'neon-lg':	'0 0 15px -3px rgb(0 0 0 / 0.1), 0 0 6px -4px rgb(0 0 0 / 0.1), inset 0 0 8px 0 rgb(0 0 0 / 0.1)',
+				'neon-xl':	'0 0 25px -5px rgb(0 0 0 / 0.1), 0 0 10px -6px rgb(0 0 0 / 0.1), inset 0 0 13px 0 rgb(0 0 0 / 0.1)',
+				'neon-2xl':	'0 0 50px -12px rgb(0 0 0 / 0.25), inset 0 0 25px 0 rgb(0 0 0 / 0.25)',
+			}
 		},
 	},
 	plugins: [animations],
