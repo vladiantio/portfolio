63a4f22d7826bf9e7589a58724e7d2c96fb7c31e
Vladimir Antonio F. C. <50648871+vladiantio@users.noreply.github.com>
2025-01-20 17:29:24 -0300

    📦 Add a `monospaced` font: Inconsolata

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -22,6 +22,7 @@ export default {
 			},
 			fontFamily: {
 				sans: ["var(--font-sans)", ...defaultTheme.fontFamily.sans],
+				mono: ["var(--font-mono)", ...defaultTheme.fontFamily.mono],
 			},
 			boxShadow: {
 				'center-sm':	'0 0 2px 0 rgb(0 0 0 / 0.05)',
