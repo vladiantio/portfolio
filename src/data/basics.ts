5824c09800c2caeae23fce0af406c174b53a1b40
Vladimir Antonio F. C. <50648871+vladiantio@users.noreply.github.com>
2025-01-27 18:05:58 -0300

    📦 Add contact email

--- src/data/basics.ts
+++ src/data/basics.ts
@@ -2,7 +2,10 @@ export default {
   name: "Vladimir Antonio Figueroa Carvajal",
   shortname: "Vladimir Antonio F. C.",
   username: "vladiantio",
-  email: "contact@vladiantio.com",
+  email: {
+    en: "contact@vladiantio.com",
+    es: "contacto@vladiantio.com",
+  },
   summary: {
     en: "+4 years of experience in developing website and application development.",
     es: "+4 años de experiencia en desarrollo de sitios y aplicaciones web.",
