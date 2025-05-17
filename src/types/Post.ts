6fdf18c858264c2d3d55ab85fcd2a7f3a823aaca
Vladimir Antonio F. C. <50648871+vladiantio@users.noreply.github.com>
2025-05-17 19:20:21 -0400

    📦 feat(Blog): refactor post sorting and transformation logic for improved structure, and include next and previous post references

--- src/types/Post.ts
+++ src/types/Post.ts
@@ -1,4 +1,8 @@
 import type { CollectionEntry } from "astro:content";
 import type { Locale } from "@/i18n/constants";
 
-export type PostEntry = CollectionEntry<'blog'> & { lang: Locale };
+export type PostEntry = CollectionEntry<'blog'> & {
+  lang: Locale;
+  next?: PostEntry;
+  previous?: PostEntry;
+};
