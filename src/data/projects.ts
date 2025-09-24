a75538a45590ccbd3d655c8bf24307467b157ac7
Vladimir Antonio F. C. <50648871+vladiantio@users.noreply.github.com>
2025-09-24 03:25:30 -0300

    🎨 style(projects): standardize quotation marks in project data for consistency

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,7 +1,7 @@
-import aforshow from '@/assets/projects/aforshow.jpg';
-import jsconfEs from '@/assets/projects/jsconf-es.jpg';
-import happyfoto from '@/assets/projects/happyfoto.jpg';
-import portalinversionista from '@/assets/projects/portalinversionista.jpg';
+import aforshow from "@/assets/projects/aforshow.jpg";
+import jsconfEs from "@/assets/projects/jsconf-es.jpg";
+import happyfoto from "@/assets/projects/happyfoto.jpg";
+import portalinversionista from "@/assets/projects/portalinversionista.jpg";
 
 interface Project {
   name: string;
@@ -31,7 +31,7 @@ export default [
       en: "A programming event made for developers by developers to give them the opportunity to give a talk.",
       es: "Un evento de programación creado por desarrolladores para desarrolladores con el fin de darles la oportunidad de dar una charla.",
     },
-    technologies: ['Astro', 'Tailwind CSS'],
+    technologies: ["Astro", "Tailwind CSS"],
     url: "https://afor.show/",
     urlCode: "https://github.com/Afordin/aforshow-2025",
     urlContributions: "https://github.com/Afordin/aforshow-2025/commits/main/?author=vladiantio",
@@ -46,7 +46,7 @@ export default [
       en: "International JavaScript Conference to be held on March 1, 2025 at La Nave, Madrid, Spain.",
       es: "Conferencia internacional de JavaScript que se celebrará el 1 de Marzo de 2025 en La Nave, Madrid, España.",
     },
-    technologies: ['Astro', 'Tailwind CSS'],
+    technologies: ["Astro", "Tailwind CSS"],
     url: "https://jsconf.es/",
     urlCode: "https://github.com/midudev/jsconf.es",
     urlContributions: "https://github.com/midudev/jsconf.es/commits/main/?author=vladiantio",
@@ -61,7 +61,7 @@ export default [
       en: "Photo booking service located in Las Condes with delivery to Santiago and more regions of Chile.",
       es: "Servicio de enmarcado de fotografías ubicado en Las Condes con envío a domicilio a Santiago y a más regiones de Chile.",
     },
-    technologies: ['Vue', 'WordPress'],
+    technologies: ["Vue", "WordPress"],
     url: "https://happyfoto.cl/",
     themeColor: "#00eac7",
   },
@@ -74,7 +74,7 @@ export default [
       en: "Property booking platform for investments in Chile. +1.000 registered customers.",
       es: "Plataforma para reservar propiedades de inversión en Chile. +1.000 clientes registrados.",
     },
-    technologies: ['Angular', 'Bootstrap', '.NET'],
+    technologies: ["Angular", "Bootstrap", ".NET"],
     madeAt: "Lecaros Group",
     url: "https://portalinversionista.com/",
     themeColor: "#03a7e2",
