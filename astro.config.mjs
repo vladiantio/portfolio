363d774fd8951ce551e8ca5e0991ca0a8b369756
Vladimir Antonio F. C. <50648871+vladiantio@users.noreply.github.com>
2025-10-04 05:44:31 -0300

    ✨ feat(markdown): add rehype-autolink-headings and rehypeHeadingIds for enhanced heading linking; update styles for link visibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,10 @@ import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
 
 // Markdown Plugins
+import { rehypeHeadingIds } from "@astrojs/markdown-remark";
+import { fromHtmlIsomorphic } from "hast-util-from-html-isomorphic";
 import { remarkReadingTime } from "./plugins/remark-reading-time.mjs";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeExternalLinks from "rehype-external-links";
 import rehypePrettyCode from "rehype-pretty-code";
 
@@ -18,6 +21,20 @@ const themes = {
   dark: "github-dark",
 };
 const transformers = [transformerColorizedBrackets()];
+
+/** @type {import("rehype-autolink-headings").Options} */
+const autolinkHeadingsOptions = {
+  content: /** @type {Array<ElementContent>} */ (
+    fromHtmlIsomorphic(
+      `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="4" x2="20" y1="9" y2="9"/><line x1="4" x2="20" y1="15" y2="15"/><line x1="10" x2="8" y1="3" y2="21"/><line x1="16" x2="14" y1="3" y2="21"/></svg>`,
+      { fragment: true }
+    ).children
+  ),
+  properties: {
+    "aria-hidden": "true",
+    class: "link-heading",
+  },
+};
 
 /** @type {import("rehype-external-links").Options} */
 const externalLinksOptions = {
@@ -61,6 +78,8 @@ export default defineConfig({
   markdown: {
     remarkPlugins: [remarkReadingTime],
     rehypePlugins: [
+      rehypeHeadingIds,
+      [rehypeAutolinkHeadings, autolinkHeadingsOptions],
       [rehypeExternalLinks, externalLinksOptions],
       [rehypePrettyCode, prettyCodeOptions],
     ],
