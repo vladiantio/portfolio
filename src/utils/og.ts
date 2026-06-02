fd7aff8a6d1cc28e01ac72a249dc1ae5a709b9f0
Vladimir Antonio F. C. <50648871+vladiantio@users.noreply.github.com>
2026-06-02 09:01:24 -0400

    style(utils): format @/utils/og.ts with semicolons

--- src/utils/og.ts
+++ src/utils/og.ts
@@ -1,86 +1,82 @@
-import { experimental_AstroContainer } from "astro/container"
-import fs from "fs/promises"
-import { decode } from "html-entities"
-import satori, { type Font } from "satori"
-import { html } from "satori-html"
-import sharp from "sharp"
+import { experimental_AstroContainer } from "astro/container";
+import fs from "fs/promises";
+import { decode } from "html-entities";
+import satori, { type Font } from "satori";
+import { html } from "satori-html";
+import sharp from "sharp";
 
-type VNode = ReturnType<typeof html>
+type VNode = ReturnType<typeof html>;
 
 function unescapeHTML(node: VNode) {
-  const children = node?.props?.children
+  const children = node?.props?.children;
   if (!children) {
-    return
+    return;
   } else if (Array.isArray(children)) {
     for (const n of children) {
-      unescapeHTML(n)
+      unescapeHTML(n);
     }
-  } else if (typeof children === 'object') {
-    unescapeHTML(children)
-  } else if (typeof children === 'string') {
-    node.props.children = decode(children)
+  } else if (typeof children === "object") {
+    unescapeHTML(children);
+  } else if (typeof children === "string") {
+    node.props.children = decode(children);
   }
 }
 
-type AstroRenderParameters = Parameters<typeof experimental_AstroContainer.prototype.renderToString>
+type AstroRenderParameters = Parameters<
+  typeof experimental_AstroContainer.prototype.renderToString
+>;
 
 export const OG = async (...parameters: AstroRenderParameters) => {
   const fonts: Font[] = [
     {
       name: "Figtree",
-      data: await fs.readFile(
-        "./src/assets/og/fonts/Figtree-Regular.ttf"
-      ),
+      data: await fs.readFile("./src/assets/og/fonts/Figtree-Regular.ttf"),
       weight: 400,
     },
     {
       name: "Figtree",
-      data: await fs.readFile(
-        "./src/assets/og/fonts/Figtree-Bold.ttf"
-      ),
+      data: await fs.readFile("./src/assets/og/fonts/Figtree-Bold.ttf"),
       weight: 700,
     },
     {
       name: "Figtree",
-      data: await fs.readFile(
-        "./src/assets/og/fonts/Figtree-Black.ttf"
-      ),
+      data: await fs.readFile("./src/assets/og/fonts/Figtree-Black.ttf"),
       weight: 900,
     },
-  ]
-  const container = await experimental_AstroContainer.create()
-  const template = await container.renderToString(...parameters)
+  ];
+  const container = await experimental_AstroContainer.create();
+  const template = await container.renderToString(...parameters);
 
   return {
     async toSvg() {
-      const node = html(template)
-      unescapeHTML(node)
+      const node = html(template);
+      unescapeHTML(node);
       return await satori(node, {
         width: 1080,
         height: 567,
         fonts,
-      })
+      });
     },
     async toImage() {
-      const svg = await this.toSvg()
+      const svg = await this.toSvg();
       return await sharp(Buffer.from(svg))
         .jpeg({
           mozjpeg: true,
           quality: 85,
           progressive: true,
         })
-        .toBuffer()
+        .toBuffer();
     },
     async toResponse() {
-      const image = await this.toImage()
-      const bytes = new Uint8Array(image)
+      const image = await this.toImage();
+      const bytes = new Uint8Array(image);
       return new Response(bytes, {
         headers: {
           "Content-Type": "image/jpeg",
           "Content-Length": image.length.toString(),
           "Cache-Control": "public, max-age=31536000, immutable",
         },
-      })
+      });
     },
-  }
-}
+  };
+};
