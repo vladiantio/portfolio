8479c48f94190946ecd68ffd934e2fb8cfaf2c0e
Vladimir Antonio F. C. <50648871+vladiantio@users.noreply.github.com>
2026-06-04 09:43:05 -0400

    chore(eslint): add jsx-a11y recommended rules to improve accessibility linting in Astro

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,5 @@
 import eslintPluginAstro from "eslint-plugin-astro";
 export default [
-  // add more generic rule sets here, such as:
-  // js.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
-  {
-    rules: {
-      // override/add rules settings here, such as:
-      // "astro/no-set-html-directive": "error"
-    },
-  },
+  ...eslintPluginAstro.configs["jsx-a11y-recommended"],
 ];
